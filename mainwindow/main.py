ca2ad4af32d0eb0bcf0cfd114120f504087c5c13
ChatNoir007 <lipiec.w@protonmail.com>
2025-03-17 08:26:46 +0100

    mainwindow - funkcja kwadratowa

--- mainwindow/main.py
+++ mainwindow/main.py
@@ -1,4 +1,7 @@
 import random
+
+import pyqtgraph
+from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
 import sys
 from PyQt6.uic import loadUi
@@ -6,9 +9,8 @@ from PyQt6.uic import loadUi
 class Dialog(QDialog):
     def __init__(self, main_window):
         super().__init__()
-        loadUi("dialog.ui", self)
+        uic.loadUi("dialog.ui", self)
         self.main_window = main_window
-        self.pushButton.clicked.connect(self.liniowa)
 
     def liniowa(self):
         try:
@@ -27,15 +29,40 @@ class Dialog(QDialog):
         except:
             self.main_window.label_wynik.setText("złe wartości")
 
+class KwadratowaDialog(QDialog):
+    def __init__(self, main_window):
+        super().__init__()
+        uic.loadUi("kwadratowa.ui", self)
+        self.main_window = main_window
+        self.pushButton.clicked.connect(self.kwadratowa)
+
+
+    def kwadratowa(self):
+        self.a = int(self.lineEdit.text())
+        self.b = int(self.lineEdit_2.text())
+        self.c = int(self.lineEdit_3.text())
+        self.x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.y = []
+        for i in self.x:
+            self.y.append((self.a * i) * (self.a * i) + self.b * i + self.c)
+        self.kwadratowaWidget.plot(self.x, self.y)
+
+
 
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        loadUi("mainwindow.ui", self)
+        uic.loadUi("mainwindow.ui", self)
 
         self.pushButton.clicked.connect(self.btnGuess)
         self.number = random.randint(1, 50)
         self.actionLiniowa.triggered.connect(self.initLiniowa)
+        self.actionKwadratowa.triggered.connect(self.initKwadratowa)
+
+        self.a = 0
+        self.b = 0
+        self.kwadratowaWidget = pyqtgraph.PlotWidget()
+        self.setCentralWidget(self.kwadratowaWidget)
 
     def btnGuess(self):
         message = QMessageBox(self)
@@ -56,6 +83,17 @@ class Window(QMainWindow):
     def initLiniowa(self):
         dialog = Dialog(self)
         dialog.exec()
+
+    def initKwadratowa(self):
+        self.KwadratowaDialog = KwadratowaDialog(self)
+        self.KwadratowaDialog.exec()
+
+
+
+
+
+
+
 
 
 app = QApplication(sys.argv)
